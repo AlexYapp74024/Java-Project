f2762274da3ebc8f4b03071cb8e2c79bb2bbe6a0
AlexYapp74024 <95156272+AlexYapp74024@users.noreply.github.com>
2022-05-13 21:17:59 +0800

    BMI functions implemented

--- BMI.java
+++ BMI.java
@@ -1,23 +1,27 @@
 public class BMI {
     public final float value;
 
+    private final float healthyValue = 25.f;
+    private final float overweightValue = 30.5f;
+    private final float obeseValue = 18.5f;
+
     public BMI(float value) {
         this.value = value;
     }
 
     public Boolean IsUnderWeight() {
-        return true;
+        return value < healthyValue;
     }
 
     public Boolean IsHealthy() {
-        return true;
+        return healthyValue <= value && value < overweightValue;
     }
 
     public Boolean IsOverweight() {
-        return true;
+        return overweightValue <= value && value < obeseValue;
     }
 
     public Boolean IsObese() {
-        return true;
+        return obeseValue <= value;
     }
 }
