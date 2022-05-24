6b4f4f038e1e94d54dbea4d057522d4aaae7679b
angeline2800 <angeline_0128@hotmail.com>
2022-05-25 01:44:58 +0800

    User delete record

--- src/deleteRecord.java
+++ src/deleteRecord.java
@@ -2,8 +2,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
