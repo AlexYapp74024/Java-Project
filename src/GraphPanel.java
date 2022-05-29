636b815da6405cd4c16149e2567fde20a9cdf948
AlexYapp74024 <95156272+AlexYapp74024@users.noreply.github.com>
2022-05-29 17:51:50 +0800

    Modified Graph to handle time

--- src/GraphPanel.java
+++ src/GraphPanel.java
@@ -1,20 +1,27 @@
 import java.awt.*;
-import java.util.List;
+import java.text.*;
+import java.time.*;
+import java.time.format.*;
+import java.time.temporal.*;
 import java.util.*;
+import java.util.List;
 import javax.swing.*;
 
 public class GraphPanel extends JPanel {
 
     private static final int padding = 25;
     private static final int labelPadding = 25;
+    private static final int hatchmarkLength = 4;
+
     private static final Color lineColor = new Color(44, 102, 230, 180);
     private static final Color pointColor = new Color(100, 100, 100, 180);
     private static final Color gridColor = new Color(200, 200, 200, 200);
     private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
-    private static final int hatchmarkLength = 4;
 
-    private static final int maxXDivisions = 10;
-    private static final int numberYDivisions = 10;
+    private final int maxXDivisions = 10;
+    private final int maxYDivisions = 10;
+    private final int XDivisions;
+    private final int YDivisions;
 
     private final int minX = padding + labelPadding;
     private final int minY = padding;
@@ -35,12 +42,21 @@ public class GraphPanel extends JPanel {
         return getHeight() - 2 * padding - labelPadding;
     };
 
-    private List<Double> scores;
+    private List<LocalDateTime> x_time;
+    private List<Double> y_value;
     private Graphics2D g2;
 
-    public GraphPanel(List<Double> scores) {
-        this.scores = scores;
+    public GraphPanel(List<LocalDateTime> X_value, List<Double> Y_value) {
+        this.x_time = X_value;
+        this.y_value = Y_value;
 
+        this.XDivisions = X_value.size() < maxXDivisions ? X_value.size() : maxXDivisions;
+        this.YDivisions = Y_value.size() < maxYDivisions ? Y_value.size() : maxYDivisions;
+    }
+
+    private void draw_white_background() {
+        g2.setColor(Color.WHITE);
+        g2.fillRect(minX, minY, xAxisLength(), yAxisLength());
     }
 
     @Override
@@ -49,9 +65,7 @@ public class GraphPanel extends JPanel {
         g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // draw white background
-        g2.setColor(Color.WHITE);
-        g2.fillRect(minX, minY, xAxisLength(), yAxisLength());
+        draw_white_background();
 
         Plot_X_Axis();
         Plot_Y_Axis();
@@ -61,24 +75,22 @@ public class GraphPanel extends JPanel {
     private void Plot_X_Axis() {
         g2.setColor(Color.BLACK);
 
-        final int xSpacing = xAxisLength() / (scores.size() - 1);
-
-        for (int i = 0; i < scores.size(); i++) {
+        final int xSpacing = xAxisLength() / XDivisions;
+        final var dateFormat = DateTimeFormatter.ofPattern("dd/MM");
+        for (int i = 0; i <= XDivisions; i++) {
 
             int x = i * xSpacing + minX;
 
-            if ((i % ((int) ((scores.size() / maxXDivisions)) + 1)) == 0) {
-                // draw vertical grid lines
-                g2.setColor(gridColor);
-                g2.drawLine(x, minY, x, maxY());
-                g2.setColor(Color.BLACK);
+            // draw vertical grid lines
+            g2.setColor(gridColor);
+            g2.drawLine(x, minY, x, maxY());
+            g2.setColor(Color.BLACK);
 
-                // draw grid label
-                final String xLabel = i + "";
-                final FontMetrics metrics = g2.getFontMetrics();
-                int labelWidth = metrics.stringWidth(xLabel);
-                g2.drawString(xLabel, x - labelWidth / 2, maxY() + metrics.getHeight() + 3);
-            }
+            // draw grid label
+            final String xLabel = getMin_X_Value().plusSeconds(X_value_division() * i).format(dateFormat);
+            final FontMetrics metrics = g2.getFontMetrics();
+            int labelWidth = metrics.stringWidth(xLabel);
+            g2.drawString(xLabel, x - labelWidth / 2, maxY() + metrics.getHeight() + 3);
 
             // hatch marks
             g2.drawLine(x, maxY(), x, maxY() - hatchmarkLength);
@@ -91,19 +103,19 @@ public class GraphPanel extends JPanel {
     private void Plot_Y_Axis() {
         g2.setColor(Color.BLACK);
 
-        final int ySpacing = yAxisLength() / numberYDivisions;
+        final int ySpacing = yAxisLength() / YDivisions;
+        final DecimalFormat df = new DecimalFormat("0.00");
 
-        for (int i = 0; i < numberYDivisions + 1; i++) {
+        for (int i = 0; i < YDivisions + 1; i++) {
             int y = maxY() - i * ySpacing;
 
             // draw grid lines
-            if (scores.size() > 0) {
+            if (y_value.size() > 0) {
                 g2.setColor(gridColor);
                 g2.drawLine(minX, y, maxX(), y);
                 g2.setColor(Color.BLACK);
-                String yLabel = ((int) ((getMinScore()
-                        + (getMaxScore() - getMinScore()) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
 
+                String yLabel = df.format(getMin_Y_Value() + Y_value_division() * i / YDivisions);
                 FontMetrics metrics = g2.getFontMetrics();
                 int labelWidth = metrics.stringWidth(yLabel);
                 g2.drawString(yLabel, minX - labelWidth - 5, y + (metrics.getHeight() / 2) - 3);
@@ -119,13 +131,13 @@ public class GraphPanel extends JPanel {
 
     private void PlotPoints() {
 
-        final double xScale = xAxisLength() / (scores.size() - 1);
-        final double yScale = yAxisLength() / (getMaxScore() - getMinScore());
+        final double xScale = xAxisLength() / (y_value.size() - 1);
+        final double yScale = yAxisLength() / (getMax_Y_Value() - getMin_Y_Value());
 
         List<Point> graphPoints = new ArrayList<>();
-        for (int i = 0; i < scores.size(); i++) {
+        for (int i = 0; i < y_value.size(); i++) {
             final int x = (int) (i * xScale + minX);
-            final int y = (int) ((getMaxScore() - scores.get(i)) * yScale + minY);
+            final int y = (int) ((getMax_Y_Value() - y_value.get(i)) * yScale + minY);
             graphPoints.add(new Point(x, y));
         }
 
@@ -154,29 +166,27 @@ public class GraphPanel extends JPanel {
         }
     }
 
-    private double getMinScore() {
-        double minScore = Double.MAX_VALUE;
-        for (Double score : scores) {
-            minScore = Math.min(minScore, score);
-        }
-        return minScore;
+    private double Y_value_division() {
+        return (getMin_Y_Value() - getMax_Y_Value()) / YDivisions;
     }
 
-    private double getMaxScore() {
-        double maxScore = Double.MIN_VALUE;
-        for (Double score : scores) {
-            maxScore = Math.max(maxScore, score);
-        }
-        return maxScore;
+    private long X_value_division() {
+        return ChronoUnit.SECONDS.between(getMin_X_Value(), getMax_X_Value()) / XDivisions;
     }
 
-    public void setScores(List<Double> scores) {
-        this.scores = scores;
-        invalidate();
-        this.repaint();
+    private LocalDateTime getMin_X_Value() {
+        return Collections.min(x_time);
     }
 
-    public List<Double> getScores() {
-        return scores;
+    private LocalDateTime getMax_X_Value() {
+        return Collections.max(x_time);
+    }
+
+    private double getMin_Y_Value() {
+        return Collections.min(y_value);
+    }
+
+    private double getMax_Y_Value() {
+        return Collections.max(y_value);
     }
 }
